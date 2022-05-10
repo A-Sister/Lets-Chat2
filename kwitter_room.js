9cd70246f65258063b5e7482c0c237d16e6b24aa
A-Sister <92495146+A-Sister@users.noreply.github.com>
2022-05-09 20:00:48 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAimWDFNQzKo1LvuWWGQIznNTJWkfZBwr0",
+    authDomain: "lets-chat-d9a95.firebaseapp.com",
+    databaseURL: "https://lets-chat-d9a95-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-d9a95",
+    storageBucket: "lets-chat-d9a95.appspot.com",
+    messagingSenderId: "454304491898",
+    appId: "1:454304491898:web:cd0bfbadacdb3ec28d0ec3"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
